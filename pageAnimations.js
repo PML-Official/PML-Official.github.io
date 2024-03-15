2d10c63837f6c9d1594b1f1a1de0160d987b0198
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2024-03-15 10:17:35 -0400

    Adding Text

--- pageAnimations.js
+++ pageAnimations.js
@@ -1,5 +1,28 @@
 
-
+// Function to check if an element is in the viewport
+//function isInViewport(element) {
+  //  const rect = element.getBoundingClientRect();
+  //  return (
+  //    rect.top >= 0 &&
+  //    rect.left >= 0 &&
+    //  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      //rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//    );
+//  }
+  
+  // Function to handle scroll event
+//  function handleScroll() {
+//    const animatedBlock = document.getElementById('animatedBlock');
+//    if (isInViewport(animatedBlock)) {
+ //     animatedBlock.classList.add('animated');
+      // Remove the scroll event listener once the animation is triggered
+ //     window.removeEventListener('scroll', handleScroll);
+//    }
+//  }
+  
+  // Add scroll event listener
+//  window.addEventListener('scroll', handleScroll);
+  
 
 
 
