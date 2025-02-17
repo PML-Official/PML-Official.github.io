2170c2515e6dbc7735eca380996efbc787f765aa
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2025-02-17 11:32:12 -0500

    Rebuild 1

--- pageAnimations.js
+++ pageAnimations.js
@@ -1,55 +1,10 @@
 // First page animation - section 1: What is PML?
 
-let animationTriggered = false;
+document.addEventListener('DOMContentLoaded', (event) => {
 
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
 
-function handleScroll() {
-    const section1Text = document.getElementById('section1Text');
-    const redline = document.getElementById('redline');
-    const button = document.getElementById('button2');
 
-    if (animationTriggered) {
-        return;
-    }
-
-    if (isInViewport(section1Text)) {
-        section1Text.classList.add('section1-animate');
-        animationTriggered = true;
-    } else {
-      section1Text.classList.remove('section1-animate')
-    }
-
-    if (isInViewport(redline)) {
-        redline.classList.add('redline-animate');
-        animationTriggered = true; 
-    } else {
-      redline.classList.remove('redline-animate')
-    }
-
-    if (isInViewport(button)) {
-        button.classList.add('button-animate');
-        animationTriggered = true;
-    } else {
-        button.classList.remove('button-animate');
-    }
-
-    if (animationTriggered) {
-        window.removeEventListener('scroll', handleScroll);
-    }
-}
-window.addEventListener('scroll', handleScroll);
-
-handleScroll();
-
+});
 
   
 
@@ -64,12 +19,12 @@ handleScroll();
    const navLogo = document.querySelector('.navlogo');
 
    function toggleNavigationAndLogo() {
-    if (window.scrollY > 0) {
+    if (window.scrollY > 39) {
         navigation.style.height = '70px';
-        navLogo.style.height = '70px';
+        navLogo.style.opacity = '100';
     } else {
         navigation.style.height = '100px';
-        navLogo.style.height = '100px';
+        navLogo.style.opacity = '0';
     }
 }
 
@@ -96,6 +51,7 @@ handleScroll();
                       closeIcon.style.display = 'none';
                   }
               };
+
 
 
 
