e2cefb0836f11d60d7a7a3e056afb1583528f128
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2025-02-18 18:56:58 -0500

    adding nonfuntional Run button

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-DOM
+    const backhome = document.getElementById('backtohome');
+
+    backhome.addEventListener('click', () => {
+        window.location.href = "https://pml-official.github.io"
+    });
+
+
+});
 
             
