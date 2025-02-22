4ccfad196f94ece42b766b7bb9f7352d2948b341
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2025-02-21 21:15:12 -0500

    Adding Home dash overlay

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -1,4 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
+
+document.addEventListener('DOMContentLoaded', () =>  {
+
+// Save Button
+
     const textEditor = document.getElementById('editor');
     const saveAlert = document.getElementById('saveAlert');
     const save = document.getElementById('save');
@@ -16,6 +20,53 @@ document.addEventListener('DOMContentLoaded', () => {
     textEditor.addEventListener("paste", showAlert);
 
     save.addEventListener("click", hideAlert);
+
+// Back to website Button
+
+    const backhome = document.getElementById('backtohome');
+
+    backhome.addEventListener('click', () => {
+        let result = confirm("If you proceed without saving, your progress will be lost");
+        if (result) {
+            window.location.href = "https://pml-official.github.io"
+        } else {
+            // Do nothing
+        }
+        
+    });
+
+
+
+// codeing button
+
+    const button = document.getElementById('code');
+    const overlay = document.getElementById('overlay');
+    const home = document.getElementById('home');
+
+    button.addEventListener('click',  () => {
+        overlay.style.display = 'none';
+    });
+
+    home.addEventListener('click', () => {
+        overlay.style.display = 'flex';
+    });
+
+
+
+
+
+
+
+
+// Menu Code
+/*
+    const menu = document.getElementById('sidebar');
+    const hamburg = document.getElementById('open');
+    const close = document.getElementById('close');
+    const overlay = document.getElementById('overlay');
+    const dashboard = document.getElementById('dashboardTop');
+*/
+
 
 
 
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const backhome = document.getElementById('backtohome');
-
-    backhome.addEventListener('click', () => {
-        window.location.href = "https://pml-official.github.io"
-    });
+    
 
     
 
