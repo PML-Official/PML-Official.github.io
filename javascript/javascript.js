d0499cb54848b0a5b1946c805dadcbed77963f46
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2025-02-24 17:06:43 -0500

    shrinking nav

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,17 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    
-
-    
-
-    
-
-    
-    
-    
-    
 
 
+    
+    const logo = document.getElementById('logo1');
+        logo.addEventListener('click', () => {
+            window.location.href = 'https://pml-official.github.io';
+        });
+    
 });
 
             
