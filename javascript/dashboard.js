ce30a0bf1bcf55967d8cc589f00112c5158310c0
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2025-02-27 20:44:18 -0500

    big changes to dashboard home and sidebar

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -1,27 +1,5 @@
 document.addEventListener('DOMContentLoaded', () =>  {
-/*
-// Save Button
 
-    const textEditor = document.getElementById('editor');
-    const saveAlert = document.getElementById('saveAlert');
-    const save = document.getElementById('save');
-    const homeText = document.getElementById('homeText');
-    const codeText = document.getElementById('codeText'); 
-
-    function showAlert() {
-        saveAlert.style.display = "block";
-    };
-
-    function hideAlert() {
-        saveAlert.style.display = 'none';
-    };
-
-    textEditor.addEventListener("input", showAlert);
-    textEditor.addEventListener("keydown", showAlert);
-    textEditor.addEventListener("paste", showAlert);
-
-    save.addEventListener("click", hideAlert);
-*/
 // Back to website Button
 
     const backhome = document.getElementById('backtohome');
@@ -36,18 +14,30 @@ document.addEventListener('DOMContentLoaded', () =>  {
 const button = document.getElementById('code');
 const overlay = document.getElementById('overlay');
 const home = document.getElementById('home');
+const codeText = document.getElementById('codeText'); 
+const homeText = document.getElementById('homeText'); 
+
+
+let homeTimeout;
+let buttonTimeout;
 
 function enableHomeHover() {
     home.onmouseover = function() {
         if (overlay.style.display === 'none') {
-            home.style.backgroundColor = 'rgb(64,64,64)';
-            home.style.width = '230px';
-            homeText.style.display = 'block';
+            clearTimeout(homeTimeout);
+            home.style.backgroundColor = 'rgb(64, 64, 64)';
+            homeTimeout = setTimeout(() => {
+                if (overlay.style.display === 'none') {
+                    home.style.width = '230px';
+                    homeText.style.display = 'block';
+                }
+            }, 1000);
         }
     };
 
     home.onmouseout = function() {
         if (overlay.style.display === 'none') {
+            clearTimeout(homeTimeout);
             home.style.backgroundColor = 'rgb(16, 16, 16)';
             home.style.width = '';
             homeText.style.display = 'none';
@@ -58,21 +48,28 @@ function enableHomeHover() {
 function disableHomeHover() {
     home.onmouseover = null;
     home.onmouseout = null;
+    clearTimeout(homeTimeout);
     home.style.width = '';
-    homeText.style.display = 'none'
+    homeText.style.display = 'none';
 }
 
 function enableButtonHover() {
     button.onmouseover = function() {
         if (overlay.style.display === 'flex' || overlay.style.display === '') {
-            button.style.backgroundColor = 'rgb(64,64,64)';
-            button.style.width = '230px';
-            codeText.style.display = 'block';
+            clearTimeout(buttonTimeout);
+            button.style.backgroundColor = 'rgb(64, 64, 64)';
+            buttonTimeout = setTimeout(() => {
+                if (overlay.style.display === 'flex' || overlay.style.display === '') {
+                    button.style.width = '230px';
+                    codeText.style.display = 'block';
+                }
+            }, 1000);
         }
     };
 
     button.onmouseout = function() {
         if (overlay.style.display === 'flex' || overlay.style.display === '') {
+            clearTimeout(buttonTimeout);
             button.style.backgroundColor = 'rgb(16, 16, 16)';
             button.style.width = '';
             codeText.style.display = 'none';
@@ -83,9 +80,11 @@ function enableButtonHover() {
 function disableButtonHover() {
     button.onmouseover = null;
     button.onmouseout = null;
+    clearTimeout(buttonTimeout);
     button.style.width = '';
     codeText.style.display = 'none';
 }
+
 
 button.addEventListener('click', () => {
     overlay.style.display = 'none';
@@ -103,7 +102,10 @@ home.addEventListener('click', () => {
     enableButtonHover();
 });
 
+
 enableButtonHover();
+
+
 
 // Side Menu Hovering
 
@@ -113,68 +115,106 @@ const download = document.getElementById('download');
 const openFile = document.getElementById('openFile');
 const newFile = document.getElementById('newFile');
 
-
 const downloadText = document.getElementById('downloadText');
 const openText = document.getElementById('openText');
 const newText = document.getElementById('newText');
 const saveAsText = document.getElementById('saveAsText');
 
-// Save As
-saveas.onmouseover = function() {
-    saveas.style.backgroundColor = 'rgb(64, 64, 64)';
-    saveas.style.width = '230px';
-    saveAsText.style.display = 'block';
-};
+const dashOpen = document.getElementById('dashOpen');
+const dashCreate = document.getElementById('dashCreate');
 
+// Save As
+let saveAsTimeout;
+saveas.onmouseover = function() {
+    clearTimeout(saveAsTimeout);
+    saveAsTimeout = setTimeout(() => {
+        saveas.style.width = '230px';
+        saveAsText.style.display = 'block';
+    }, 1000);
+    saveas.style.backgroundColor = 'rgb(64, 64, 64)';
+};
 saveas.onmouseout = function() {
+    clearTimeout(saveAsTimeout);
     saveas.style.backgroundColor = '';
     saveas.style.width = '';
     saveAsText.style.display = '';
 };
 
-
 // Download
+let downloadTimeout;
 download.onmouseover = function() {
+    clearTimeout(downloadTimeout);
+    downloadTimeout = setTimeout(() => {
+        download.style.width = '230px';
+        downloadText.style.display = 'block';
+    }, 1000);
     download.style.backgroundColor = 'rgb(64, 64, 64)';
-    download.style.width = '230px';
-    downloadText.style.display = 'block';
 };
-
 download.onmouseout = function() {
+    clearTimeout(downloadTimeout);
     download.style.backgroundColor = '';
     download.style.width = '';
     downloadText.style.display = '';
 };
 
-
-//  open file
+// Open File
+let openFileTimeout;
 openFile.onmouseover = function() {
+    clearTimeout(openFileTimeout);
+    openFileTimeout = setTimeout(() => {
+        openFile.style.width = '230px';
+        openText.style.display = 'block';
+    }, 1000);
+    dashOpen.style.color = 'white';
     openFile.style.backgroundColor = 'rgb(64, 64, 64)';
-    openFile.style.width = '230px';
-    openText.style.display = 'block';
 };
-
 openFile.onmouseout = function() {
+    clearTimeout(openFileTimeout);
     openFile.style.backgroundColor = '';
     openFile.style.width = '';
     openText.style.display = '';
+    dashOpen.style.color = '';
 };
-
 
 // New File
+let newFileTimeout;
 newFile.onmouseover = function() {
+    clearTimeout(newFileTimeout);
+    newFileTimeout = setTimeout(() => {
+        newFile.style.width = '230px';
+        newText.style.display = 'block';
+    }, 1000);
+    dashCreate.style.color = 'white';
     newFile.style.backgroundColor = 'rgb(64, 64, 64)';
-    newFile.style.width = '230px';
-    newText.style.display = 'block';
 };
-
 newFile.onmouseout = function() {
+    clearTimeout(newFileTimeout);
     newFile.style.backgroundColor = '';
     newFile.style.width = '';
     newText.style.display = '';
+    dashCreate.style.color = '';
 };
 
+// Dashboard Open Existing
 
+dashOpen.onmouseover = function() {
+    openFile.style.backgroundColor = 'rgb(64, 64, 64)';
+}
+
+dashOpen.onmouseout = function() {
+    openFile.style.backgroundColor = '';
+}
+
+
+// Dashboard Create New
+
+dashCreate.onmouseover = function() {
+    newFile.style.backgroundColor = 'rgb(64, 64, 64)';
+}
+
+dashCreate.onmouseout = function() {
+    newFile.style.backgroundColor = '';
+}
 
 
 
